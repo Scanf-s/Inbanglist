6157ecf6a5b08272be2c72bf0d8f0bb747f22f53
Sullung <sullung2yo@gmail.com>
2024-06-16 01:43:57 +0900

    Git Test

--- api/app/config/urls.py
+++ api/app/config/urls.py
@@ -26,3 +26,4 @@ from django.conf import settings
 from django.conf.urls.static import static
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
