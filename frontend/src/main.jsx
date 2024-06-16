ab3a18193b6d38fba61faad720ddbd3249934e53
Skyler <123640595+Skyler85@users.noreply.github.com>
2024-06-17 00:52:54 +0900

    Views (#23)

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
+);
