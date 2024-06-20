5e1b1020f987bdfa6691ea2225aed013b908c2bb
rbwo552 <rbwo552@naver.com>
2024-06-20 21:22:21 +0900

    셀레니움 사용한 병렬 크롤링

--- scraper/app/crawling/youtube_crawling.py
+++ scraper/app/crawling/youtube_crawling.py
@@ -1,35 +1,8 @@
 import re
 import time
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-def init_driver():
-    user_info = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-    service = Service(ChromeDriverManager().install())
-
-    options = Options()
-    options.add_experimental_option("detach", True)  # Keep browser open
-    options.add_experimental_option('excludeSwitches', ['disable-popup-blocking', 'enable-automation'])  # Disable popup
-    options.add_argument("window-size=800,1280")  # Set window size
-    options.add_argument("incognito")  # Secret mode
-    # options.add_argument("--headless") # Run in background
-    options.add_argument("--mute-audio")  # Mute audio
-    options.add_argument(f"user-agent={user_info}")
-
-    new_driver = webdriver.Chrome(options=options, service=service)
-    return new_driver
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 def scroll(driver):
     elem = driver.find_element(By.TAG_NAME, "body")
@@ -65,19 +38,20 @@ def press_show_all(driver):
     first_section = contents.find_element(By.TAG_NAME, "ytd-rich-section-renderer")
     menu_container = first_section.find_element(By.ID, "menu-container")
     ActionChains(driver).move_to_element(menu_container).click().perform()
-    time.sleep(5)
+    time.sleep(2)
 
 
-def get_live_details(driver):
+def get_live_details(driver, soup):
     """
     Crawling 함수
     + 필터링 기능
     """
     page = driver.page_source
-    soup = BeautifulSoup(page, "html.parser")
+    soup = soup(page, "html.parser")
 
     thumbnail_list = [img['src'] for yt_image in soup.find_all("yt-image") for img in yt_image.find_all("img") if
                       'src' in img.attrs]
+    link_list = [link.get('href') for link in soup.find_all("a", class_="yt-simple-endpoint inline-block style-scope ytd-thumbnail") if link.get('href')]
     title_list = [title.text for title in soup.find_all("yt-formatted-string", id="video-title")]
     channel_name_list = [channel_name.text for text_container in soup.find_all("div", {"id": "text-container"}) for
                          channel_name in text_container.find_all('a')]
@@ -85,40 +59,27 @@ def get_live_details(driver):
                          soup.find_all("span", class_="inline-metadata-item style-scope ytd-video-meta-block")]
 
     # 만약 방금 종료된 방송이 있다면, 제외시켜야 하므로 정규식을 사용해서 필터링합니다.
-    filtered_data = [(thumb, title, channel, viewers) for thumb, title, channel, viewers in
-                     zip(thumbnail_list, title_list, channel_name_list, live_viewers_list) if
+    datas = [(thumb, link, title, channel, viewers) for thumb, link, title, channel, viewers in
+                     zip(thumbnail_list, link_list, title_list, channel_name_list, live_viewers_list) if
                      not re.search(r'Streamed \d+', viewers)]
 
     # dictionary list로 바꿔서 제공해줍니다.
     live_data_list = [
         {
             'thumbnail': data[0],
-            'title': data[1],
-            'channel_name': data[2],
-            'viewers': data[3],
-        } for data in filtered_data
+            'link': data[1],
+            'title': data[2],
+            'channel_name': data[3],
+            'viewers': data[4],
+        } for data in datas
     ]
 
     return live_data_list
 
-
-def main(driver):
+def output(driver, soup):
     url = "https://www.youtube.com/channel/UC4R8DWoMoI7CAwX8_LjQHig"
     driver.get(url)
-    time.sleep(5)
-
-    try:
-        press_show_all(driver)
-        scroll(driver)
-        live_data_list = get_live_details(driver)
-        for live_data in live_data_list:
-            for key, value in live_data.items():
-                print(f"{key}: {value}")
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-if __name__ == "__main__":
-    driver = init_driver()
-    main(driver)
+    time.sleep(2)
+    press_show_all(driver)
+    scroll(driver)
+    return get_live_details(driver, soup)
---
+++ scraper/app/main.py
@@ -0,0 +1,40 @@
+import time
+from crawling import afreecatv_crawling, youtube_crawling, chzzk_crawling
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+from concurrent import futures
+
+def init_driver():
+    user_info = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+    service = Service(ChromeDriverManager().install())
+
+    options = Options()
+    options.add_experimental_option("detach", True)  # Keep browser open
+    options.add_experimental_option('excludeSwitches', ['disable-popup-blocking', 'enable-automation'])  # Disable popup
+    options.add_argument("window-size=800,1280")  # Set window size
+    options.add_argument("incognito")  # Secret mode
+    options.add_argument("--headless") # Run in background
+    options.add_argument("--mute-audio")  # Mute audio
+    options.add_argument(f"user-agent={user_info}")
+
+    new_driver = webdriver.Chrome(options=options, service=service)
+    return new_driver
+
+def run():
+    with futures.ThreadPoolExecutor() as executor:
+        afreecatv_data = executor.submit(afreecatv_crawling.output, init_driver(), BeautifulSoup)
+        youtube_data = executor.submit(youtube_crawling.output, init_driver(), BeautifulSoup)
+        chzzk_data = executor.submit(chzzk_crawling.output, init_driver(), BeautifulSoup)
+
+    print(len(afreecatv_data.result()))
+    print(len(youtube_data.result()))
+    print(len(chzzk_data.result()))
+
+if __name__ == "__main__":
+    start =  time.time()
+    run()
+    end = time.time()
+    print(f"{end - start:.5f} sec")
