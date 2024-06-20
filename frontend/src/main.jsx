ae6ccecbfcd36ffdf44f07b100ff86f9b2a6567a
h22jul22 <heeju9402@gmail.com>
2024-06-20 23:54:14 +0900

    MSW를 이용한 API 테스트 (#46)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,16 @@
 import './index.css';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { enableMocking } from './mocks/enableMocking.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+async function startApp() {
+    await enableMocking();
+    ReactDOM.createRoot(document.getElementById('root')).render(
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
+}
 
-);
+startApp();
