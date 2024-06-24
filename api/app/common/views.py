604378ded2325eec6f122b3d4a3d47dcdb961b31
Sullung <105439069+Scanf-s@users.noreply.github.com>
2024-06-24 19:13:54 +0900

    CORS 문제 해결 시도 (#67)
    Co-authored-by: Sullung <sullung2yo@gmail.com>

--- api/app/common/views.py
+++ api/app/common/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class RootView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("<h1>API</h1>")
