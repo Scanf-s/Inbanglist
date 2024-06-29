33e5178590743425e92425c9efd614ca6f81df54
rbwo552 <rbwo552@naver.com>
2024-06-29 20:50:00 +0900

    update insert function

--- scraper/app/db/connector.py
+++ scraper/app/db/connector.py
@@ -1,6 +1,6 @@
 import os
-import psycopg2
-from datetime import datetime
+from psycopg2 import connect, extras, pool
+from psycopg2 import Error as DB_Error
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,37 +11,45 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 
-conn = psycopg2.connect(
+connection_pool = pool.SimpleConnectionPool(1, 10,
     host=DB_HOST,
     database=DB_NAME,
     user=DB_USER,
     password=DB_PASSWORD
 )
 
-def insert(datas):
-    truncate_query = """
-        TRUNCATE TABLE live_streaming_table RESTART IDENTITY;
-    """
+def execute_query(query, params=None, many=False):
+    conn = connection_pool.getconn()
+    try:
+        with conn.cursor() as cur:
+            if many:
+                extras.execute_values(cur, query, params)
+            else:
+                cur.execute(query, params)
+            conn.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+        conn.rollback()
+        raise
+    finally:
+        connection_pool.putconn(conn)
 
+def truncate_table():
+    truncate_query = "TRUNCATE TABLE live_streaming_table RESTART IDENTITY;"
+    execute_query(truncate_query)
+
+def insert(datas):
+    truncate_table()
     insert_query = """
-        INSERT INTO live_streaming_table (channel_name, thumbnail, concurrent_viewers, title, platform, streaming_link, channel_link, channel_description, followers, created_at, updated_at)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        INSERT INTO live_streaming_table
+        (channel_name, thumbnail, concurrent_viewers, title, platform, 
+        streaming_link, channel_link, channel_description, 
+        channel_followers, created_at, updated_at, channel_profile_image)
+        VALUES %s
     """
 
     values = [tuple(data.values()) for data in datas]
-
-    try:
-        cur = conn.cursor()
-        cur.execute(truncate_query)
-        cur.executemany(insert_query, values)
-        conn.commit()
-
-    except psycopg2.Error as e:
-        print(f"Error: {e}")
-        conn.rollback()
-    finally:
-        cur.close()
-        conn.close()
+    execute_query(insert_query, values, many=True)
 
 def select_all():
     select_query = """
@@ -49,11 +57,12 @@ def select_all():
     """
     
     try:
+        conn = connection_pool.getconn()
         cur = conn.cursor()
         cur.execute(select_query)
         rows = cur.fetchall()
         return rows
-    except psycopg2.Error as e:
+    except DB_Error as e:
         print(f"Error: {e}")
     finally:
         cur.close()
